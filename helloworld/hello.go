9be25e64ff6a5825bfa9e7fbdf7012f51cb6db54
howdy39 <tatsuya.howdy@gmail.com>
2017-10-25 21:55:03 +0900

    Add Hello, world

---
+++ helloworld/hello.go
@@ -0,0 +1,14 @@
+package helloworld
+
+import (
+	"net/http"
+	"fmt"
+)
+
+func init() {
+	http.HandleFunc("/hello", handler)
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "Hello, world!")
+}
